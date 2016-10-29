f7da2a18d3ee49ef30936e702fb2696610e0c71c
Andreas Linnert <andreas@linnertmedia.de>
2016-10-29 17:39:13 +0200

    Fixes #1 plus some code clean-up

--- src/window.js
+++ src/window.js
@@ -1,14 +1,11 @@
 'use strict';
 
-const $ = document.querySelector.bind(document),
-    $$ = document.querySelectorAll.bind(document);
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
 
-function nodeListIndexOf(list, element)
-{
-    for (let i = 0; i < list.length; i++)
-    {
-        if (list[i] === element)
-        {
+function nodeListIndexOf(list, element) {
+    for (let i = 0; i < list.length; i++) {
+        if (list[i] === element) {
             return i;
         }
     }
@@ -16,10 +13,8 @@ function nodeListIndexOf(list, element)
     return -1;
 }
 
-class Tblr
-{
-    constructor()
-    {
+class Tblr {
+    constructor() {
         this.inputArea = $('#data-input');
         this.sidebarAreaSelectElements = $$('main > aside select');
         this.sidebarAreaCheckboxElements = $$('main > aside input[type=checkbox]');
@@ -32,73 +27,58 @@ class Tblr
 
     get version() { return chrome.runtime.getManifest().version; }
 
-    prepareEvents()
-    {
-        let that = this;
-        this.inputArea.addEventListener('input', function (event) { that.convertInput(event); }, false);
-        this.inputArea.addEventListener('keydown', function (event)
-        {
+    prepareEvents() {
+        this.inputArea.addEventListener('input', (event) => this.convertInput(event), false);
+        this.inputArea.addEventListener('keydown', (event) => {
             Tblr.supportTab(event);
-            that.forwardEvents(event);
+            this.forwardEvents(event);
         }, false);
 
-        for (let i = 0; i < this.tabElements.length; i++)
-        {
-            this.tabElements[i].addEventListener('click', function (event) { Tblr.changeTab(event); }, false);
+        for (let i = 0; i < this.tabElements.length; i++) {
+            this.tabElements[i].addEventListener('click', (event) => Tblr.changeTab(event), false);
         }
 
-        for (let i = 0; i < this.sidebarAreaSelectElements.length; i++)
-        {
-            this.sidebarAreaSelectElements[i].addEventListener('change', function (event) { that.convertInput(event); }, false);
+        for (let i = 0; i < this.sidebarAreaSelectElements.length; i++) {
+            this.sidebarAreaSelectElements[i].addEventListener('change', (event) => this.convertInput(event), false);
         }
 
-        for (let i = 0; i < this.sidebarAreaCheckboxElements.length; i++)
-        {
-            this.sidebarAreaCheckboxElements[i].addEventListener('click', function (event) { that.convertInput(event); }, false);
+        for (let i = 0; i < this.sidebarAreaCheckboxElements.length; i++) {
+            this.sidebarAreaCheckboxElements[i].addEventListener('click', (event) => this.convertInput(event), false);
         }
     }
 
-    displayVersion()
-    {
+    displayVersion() {
         $('#app-version').innerText = this.version;
     }
 
-    convertInput()
-    {
-        const tableData = this.inputArea.value !== '' ? this.inputArea.value.split('\n') : [],
-            oColumnDelimiter = $('#setting-column-delimiter').value,
-            oFillEmptyCells = $('#setting-fill-empty-cells').checked,
-            oIncludeTableElement = $('#setting-include-table-element').checked,
-            oFirstRowTh = $('#setting-first-row-th').checked;
+    convertInput() {
+        const tableData = this.inputArea.value !== '' ? this.inputArea.value.split('\n') : [];
+        const oColumnDelimiter = $('#setting-column-delimiter').value;
+        const oFillEmptyCells = $('#setting-fill-empty-cells').checked;
+        const oIncludeTableElement = $('#setting-include-table-element').checked;
+        const oFirstRowTh = $('#setting-first-row-th').checked;
+        let maxColumnsCount = 0;
+        let emptyCellsCount = 0;
+        let rowCounter;
 
-        let maxColumnsCount = 0,
-            emptyCellsCount = 0,
-            rowCounter;
-
-        for (rowCounter = 0; rowCounter < tableData.length; rowCounter++)
-        {
-            const tableLineData = tableData[rowCounter].split(oColumnDelimiter),
-                indent = oIncludeTableElement ? '\t' : '',
-                tableCellTag = oFirstRowTh && rowCounter === 0 ? 'th' : 'td';
+        for (rowCounter = 0; rowCounter < tableData.length; rowCounter++) {
+            const tableLineData = tableData[rowCounter].split(oColumnDelimiter);
+            const indent = oIncludeTableElement ? '\t' : '';
+            const tableCellTag = oFirstRowTh && rowCounter === 0 ? 'th' : 'td';
             let columnCounter;
 
-            for (columnCounter = 0; columnCounter < tableLineData.length; columnCounter++)
-            {
-                if (tableLineData[columnCounter] === '')
-                {
-                    // START | fill empty cells with &nbsp;
-                    if (oFillEmptyCells)
-                    {
+            for (columnCounter = 0; columnCounter < tableLineData.length; columnCounter++) {
+                if (tableLineData[columnCounter] === '') {
+                    // fill empty cells with &nbsp;
+                    if (oFillEmptyCells) {
                         tableLineData[columnCounter] = '&nbsp;';
                     }
-                    // END | fill empty cells with &nbsp;
 
                     emptyCellsCount++;
                 }
             }
 
-            if (columnCounter > maxColumnsCount)
-            {
+            if (columnCounter > maxColumnsCount) {
                 maxColumnsCount = columnCounter;
             }
 
@@ -107,9 +87,9 @@ class Tblr
             tableData[rowCounter] = `${indent}<tr><${tableCellTag}>${rowHtml}</${tableCellTag}></tr>`;
         }
 
-        const rowsHtml = tableData.join('\n'),
-            tableHtml = '<table>\n' + rowsHtml + '\n</table>',
-            noResultText = 'No data available!\n\nPlease input some data into the textarea on the left.';
+        const rowsHtml = tableData.join('\n');
+        const tableHtml = '<table>\n' + rowsHtml + '\n</table>';
+        const noResultText = 'No data available!\n\nPlease input some data into the textarea on the left.';
 
         $('#code-output').value = tableData.length > 0 ? (oIncludeTableElement ? tableHtml : rowsHtml) : noResultText;
         $('#code-preview').innerHTML = tableHtml;
@@ -120,38 +100,33 @@ class Tblr
         $('#aside-info-empty-cells').innerText = emptyCellsCount;
     }
 
-    static supportTab(event)    
-    {
-        if (event.keyCode === 9)
-        {
+    static supportTab(event) {
+        if (event.keyCode === 9) {
             event.preventDefault();
 
-            const start = event.currentTarget.selectionStart,
-            end = event.currentTarget.selectionEnd;
+            const start = event.currentTarget.selectionStart;
+            const end = event.currentTarget.selectionEnd;
 
             event.currentTarget.value = event.currentTarget.value.substring(0, start) + '\t' + event.currentTarget.value.substring(end);
             event.currentTarget.selectionStart = event.currentTarget.selectionEnd = start + 1;
         }
     }
 
-    forwardEvents()
-    {
+    forwardEvents() {
         const evt = document.createEvent('HTMLEvents');
 
         evt.initEvent('input', false, true);
         this.inputArea.dispatchEvent(evt);
     }
 
-    static changeTab(event)
-    {
-        const clickedTab = event.currentTarget,
-            tabElement = clickedTab.parentElement.parentElement,
-            allTabs = tabElement.querySelectorAll('.tabs .tab'),
-            clickedTabIndex = nodeListIndexOf(allTabs, clickedTab),
-            allTabBodyElements = tabElement.querySelectorAll('.tab-contents .tab-body');
+    static changeTab(event) {
+        const clickedTab = event.currentTarget;
+        const tabElement = clickedTab.parentElement.parentElement;
+        const allTabs = tabElement.querySelectorAll('.tabs .tab');
+        const clickedTabIndex = nodeListIndexOf(allTabs, clickedTab);
+        const allTabBodyElements = tabElement.querySelectorAll('.tab-contents .tab-body');
 
-        for (let i = 0; i < allTabs.length; i++)
-        {
+        for (let i = 0; i < allTabs.length; i++) {
             allTabs[i].classList.remove('current');
             allTabBodyElements[i].classList.remove('current');
         }
