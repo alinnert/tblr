5d723b766d2a2d2c9c39d9d0f0678598be169f25
Andreas Linnert <andreas@linnertmedia.de>
2018-03-13 19:41:29 +0100

    Closes #4 #5 #6
    Added option 'first column is `<th>`.
    Added new separator: '| pipe'.
    Fixed bug that separator options '1 or more spaces' and
    '4 or more spaces' treat tabs as spaces.
    Some UI changes.

--- readme.md
+++ readme.md
@@ -1,6 +1,7 @@
 # tblr - Chrome Packaged App
 
 ## Description
+
 This app converts your data into a html table.
 
 You can copy data from Excel or a simlar software into this app and it generates the html code for a table with the provided data.
@@ -8,60 +9,76 @@ You can copy data from Excel or a simlar software into this app and it generates
 It's possible to change the delimiter within each line. This allows you to generate tables of CSV for instance.
 
 You can also see some statistics about your data like:
-- number of rows
-- number of columns
-- number of empty cells (cells without any data)
 
-
+* number of rows
+* number of columns
+* number of empty cells (cells without any data)
 
 ## Known issues
-It's not supported to paste tables from *Microsoft Office Excel* that include line breaks.
+
+It's not supported to paste tables from _Microsoft Office Excel_ that include line breaks.
 
 Why? There's technically no difference between line breaks and a new table row.
 
-
-
 ## Change log
 
-### 1.2.0 (29.10.2016)
-- **[NEW]** Added new column separators: '1+ spaces', '4 spaces', '4+ spaces'
-- **[CHANGE]** Slightly bigger UI text, some wording changes
-- **[FIX]** Text from HTML panel couldn't be selected anymore.
+### 1.3.0 ‒ 2018-03-13
 
-### 1.1.5 (23.05.2015)
-- **[FIX]** GitHub links were not working. In Chrome Apps `target="_blank"` is necessary for links to work.
+* **[NEW]** Added new option 'First column is `<th>`' (closes #6)
+* **[NEW]** Added new column separator: '| pipe' (closes #4)
+* **[CHANGE]** Some UI changes
+* **[FIX]** Separators '1 or more spaces' and '4 or more spaces' don't treat tabs as spaces any longer (closes #5)
 
-### 1.1.4 (23.05.2015)
-- **[CHANGE]** Updated links in the sidebar.
-- **[FIX]** Sidebar scrolls correctly now.
-- **[FIX]** Get app version dynamically from the manifest file.
-- **[FIX]** Using ES6 classes.
+### 1.2.0 ‒ 2016-10-29
 
-### 1.1.3 (21.05.2015)
-- **[FIX]** Fixed a typo in the source code that prevented the app from working.
+* **[NEW]** Added new column separators: '1+ spaces', '4 spaces' (closes #2), '4+ spaces' (closes #3)
+* **[CHANGE]** Slightly bigger UI text, some wording changes
+* **[FIX]** Text from HTML panel couldn't be selected anymore. (closes #1)
 
-### 1.1.2 (21.05.2015)
-- **[FIX]** Link "Change log" points to GitHub repository instead of the old log file now.
+### 1.1.5 ‒ 2015-05-23
 
-### 1.1.1 (21.05.2015)
-- **[FIX]** Fixed warnings and errors pointed out by PhpStorm.
+* **[FIX]** GitHub links were not working. In Chrome Apps `target="_blank"` is necessary for links to work.
 
-### 1.1.0 (21.05.2015)
-- **[NEW]** Added option to disable output of the `<table>` element.
-- **[NEW]** Added option to define cells of the first row as `<th>` elements.
-- **[NEW]** Added preview of the table
-- **[CHANGE]** Added color to the window frame and changed input and sidebar colors.
-- **[FIX]** Updated and optimized the source code since Chrome now supports `let` and `const`.
+### 1.1.4 ‒ 2015-05-23
 
-### 1.0.3 (~ June 2014)
-- **[FIX]** The window now remembers its size and position.
+* **[CHANGE]** Updated links in the sidebar.
+* **[FIX]** Sidebar scrolls correctly now.
+* **[FIX]** Get app version dynamically from the manifest file.
+* **[FIX]** Using ES6 classes.
 
-### 1.0.2 (~ June 2014)
-- **[NEW]** Added links to a changelog file (which is not existing anymore).
-- **[FIX]** More reliable event handling for the options.
+### 1.1.3 ‒ 2015-05-21
 
-### 1.0.1 (~ June 2014)
-- **[NEW]** Font "Ubuntu Mono" included so everyone can see it.
+* **[FIX]** Fixed a typo in the source code that prevented the app from working.
 
-### 1.0.0 (~ June 2014)
-- **[NEW]** **Everything** :D ...it was the first version.
+### 1.1.2 ‒ 2015-05-21
+
+* **[FIX]** Link "Change log" points to GitHub repository instead of the old log file now.
+
+### 1.1.1 2015-05-21
+
+* **[FIX]** Fixed warnings and errors pointed out by PhpStorm.
+
+### 1.1.0 2015-05-21
+
+* **[NEW]** Added option to disable output of the `<table>` element.
+* **[NEW]** Added option to define cells of the first row as `<th>` elements.
+* **[NEW]** Added preview of the table
+* **[CHANGE]** Added color to the window frame and changed input and sidebar colors.
+* **[FIX]** Updated and optimized the source code since Chrome now supports `let` and `const`.
+
+### 1.0.3 2014-06
+
+* **[FIX]** The window now remembers its size and position.
+
+### 1.0.2 2014-06
+
+* **[NEW]** Added links to a changelog file (which is not existing anymore).
+* **[FIX]** More reliable event handling for the options.
+
+### 1.0.1 2014-06
+
+* **[NEW]** Font "Ubuntu Mono" included so everyone can see it.
+
+### 1.0.0 2014-06
+
+* **[NEW]** **Everything** :D ...it was the first version.
--- src/window.js
+++ src/window.js
@@ -1,142 +1,188 @@
-'use strict';
+'use strict'
 
-const $ = document.querySelector.bind(document);
-const $$ = document.querySelectorAll.bind(document);
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
 
 function nodeListIndexOf(list, element) {
-    for (let i = 0; i < list.length; i++) {
-        if (list[i] === element) {
-            return i;
-        }
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === element) {
+      return i
     }
+  }
 
-    return -1;
+  return -1
 }
 
 class Tblr {
-    constructor() {
-        this.inputArea = $('#data-input');
-        this.sidebarAreaSelectElements = $$('main > aside select');
-        this.sidebarAreaCheckboxElements = $$('main > aside input[type=checkbox]');
-        this.tabElements = $$('.tab-element .tabs .tab');
+  constructor() {
+    this.inputArea = $('#data-input')
+    this.sidebarAreaSelectElements = $$('main > aside select')
+    this.sidebarAreaCheckboxElements = $$('main > aside input[type=checkbox]')
+    this.tabElements = $$('.tab-element .tabs .tab')
 
-        this.prepareEvents();
-        this.displayVersion();
-        this.convertInput();
+    this.prepareEvents()
+    this.displayVersion()
+    this.convertInput()
+  }
+
+  get version() {
+    return chrome.runtime.getManifest().version
+  }
+
+  prepareEvents() {
+    this.inputArea.addEventListener(
+      'input',
+      event => this.convertInput(event),
+      false
+    )
+    this.inputArea.addEventListener(
+      'keydown',
+      event => {
+        Tblr.supportTab(event)
+        this.forwardEvents(event)
+      },
+      false
+    )
+
+    for (let i = 0; i < this.tabElements.length; i++) {
+      this.tabElements[i].addEventListener(
+        'click',
+        event => Tblr.changeTab(event),
+        false
+      )
     }
 
-    get version() { return chrome.runtime.getManifest().version; }
+    for (let i = 0; i < this.sidebarAreaSelectElements.length; i++) {
+      this.sidebarAreaSelectElements[i].addEventListener(
+        'change',
+        event => this.convertInput(event),
+        false
+      )
+    }
 
-    prepareEvents() {
-        this.inputArea.addEventListener('input', (event) => this.convertInput(event), false);
-        this.inputArea.addEventListener('keydown', (event) => {
-            Tblr.supportTab(event);
-            this.forwardEvents(event);
-        }, false);
+    for (let i = 0; i < this.sidebarAreaCheckboxElements.length; i++) {
+      this.sidebarAreaCheckboxElements[i].addEventListener(
+        'click',
+        event => this.convertInput(event),
+        false
+      )
+    }
+  }
 
-        for (let i = 0; i < this.tabElements.length; i++) {
-            this.tabElements[i].addEventListener('click', (event) => Tblr.changeTab(event), false);
+  displayVersion() {
+    $('#app-version').innerText = this.version
+  }
+
+  convertInput() {
+    const tableData =
+      this.inputArea.value !== '' ? this.inputArea.value.split('\n') : []
+    const settingColumnDelimiterValue = $('#setting-column-delimiter').value
+    const oColumnDelimiter = settingColumnDelimiterValue.match(/^rx:/)
+      ? new RegExp(settingColumnDelimiterValue.replace(/^rx:/, ''))
+      : settingColumnDelimiterValue
+    const oFillEmptyCells = $('#setting-fill-empty-cells').checked
+    const oIncludeTableElement = $('#setting-include-table-element').checked
+    const oFirstRowTh = $('#setting-first-row-th').checked
+    const oFirstColTh = $('#setting-first-col-th').checked
+    let maxColumnsCount = 0
+    let emptyCellsCount = 0
+    let rowCounter
+
+    for (rowCounter = 0; rowCounter < tableData.length; rowCounter++) {
+      const indent = oIncludeTableElement ? '\t' : ''
+      let tableLineData = tableData[rowCounter].split(oColumnDelimiter)
+      let colCounter
+      console.log('row')
+      
+      for (colCounter = 0; colCounter < tableLineData.length; colCounter++) {
+        console.log('col')
+        if (tableLineData[colCounter] === '') {
+          // fill empty cells with &nbsp;
+          if (oFillEmptyCells) {
+            tableLineData[colCounter] = '&nbsp;'
+          }
+
+          emptyCellsCount++
         }
 
-        for (let i = 0; i < this.sidebarAreaSelectElements.length; i++) {
-            this.sidebarAreaSelectElements[i].addEventListener('change', (event) => this.convertInput(event), false);
+        if (
+          (oFirstRowTh && rowCounter === 0) ||
+          (oFirstColTh && colCounter === 0)
+        ) {
+          tableLineData[colCounter] = `<th>${tableLineData[colCounter]}</th>`
+        } else {
+          tableLineData[colCounter] = `<td>${tableLineData[colCounter]}</td>`
         }
+      }
 
-        for (let i = 0; i < this.sidebarAreaCheckboxElements.length; i++) {
-            this.sidebarAreaCheckboxElements[i].addEventListener('click', (event) => this.convertInput(event), false);
-        }
+      if (colCounter > maxColumnsCount) {
+        maxColumnsCount = colCounter
+      }
+
+      const rowHtml = tableLineData.join('')
+
+      tableData[
+        rowCounter
+      ] = `${indent}<tr>${rowHtml}</tr>`
     }
 
-    displayVersion() {
-        $('#app-version').innerText = this.version;
+    const rowsHtml = tableData.join('\n')
+    const tableHtml = '<table>\n' + rowsHtml + '\n</table>'
+    const noResultText =
+      'No data available!\n\nPlease input some data into the textarea on the left.'
+
+    $('#code-output').textContent =
+      tableData.length > 0
+        ? oIncludeTableElement ? tableHtml : rowsHtml
+        : noResultText
+    $('#code-preview').innerHTML = tableHtml
+
+    // update information section
+    $('#aside-info-lines').innerText = rowCounter
+    $('#aside-info-columns').innerText = maxColumnsCount
+    $('#aside-info-empty-cells').innerText = emptyCellsCount
+  }
+
+  static supportTab(event) {
+    if (event.keyCode === 9) {
+      event.preventDefault()
+
+      const start = event.currentTarget.selectionStart
+      const end = event.currentTarget.selectionEnd
+
+      event.currentTarget.value =
+        event.currentTarget.value.substring(0, start) +
+        '\t' +
+        event.currentTarget.value.substring(end)
+      event.currentTarget.selectionStart = event.currentTarget.selectionEnd =
+        start + 1
+    }
+  }
+
+  forwardEvents() {
+    const evt = document.createEvent('HTMLEvents')
+
+    evt.initEvent('input', false, true)
+    this.inputArea.dispatchEvent(evt)
+  }
+
+  static changeTab(event) {
+    const clickedTab = event.currentTarget
+    const tabElement = clickedTab.parentElement.parentElement
+    const allTabs = tabElement.querySelectorAll('.tabs .tab')
+    const clickedTabIndex = nodeListIndexOf(allTabs, clickedTab)
+    const allTabBodyElements = tabElement.querySelectorAll(
+      '.tab-contents .tab-body'
+    )
+
+    for (let i = 0; i < allTabs.length; i++) {
+      allTabs[i].classList.remove('current')
+      allTabBodyElements[i].classList.remove('current')
     }
 
-    convertInput() {
-        const tableData = this.inputArea.value !== '' ? this.inputArea.value.split('\n') : [];
-        const settingColumnDelimiterValue = $('#setting-column-delimiter').value;
-        const oColumnDelimiter = settingColumnDelimiterValue.match(/^rx:/)
-            ? new RegExp(settingColumnDelimiterValue.replace(/^rx:/, ''))
-            : settingColumnDelimiterValue;
-        const oFillEmptyCells = $('#setting-fill-empty-cells').checked;
-        const oIncludeTableElement = $('#setting-include-table-element').checked;
-        const oFirstRowTh = $('#setting-first-row-th').checked;
-        let maxColumnsCount = 0;
-        let emptyCellsCount = 0;
-        let rowCounter;
-
-        for (rowCounter = 0; rowCounter < tableData.length; rowCounter++) {
-            const tableLineData = tableData[rowCounter].split(oColumnDelimiter);
-            const indent = oIncludeTableElement ? '\t' : '';
-            const tableCellTag = oFirstRowTh && rowCounter === 0 ? 'th' : 'td';
-            let columnCounter;
-
-            for (columnCounter = 0; columnCounter < tableLineData.length; columnCounter++) {
-                if (tableLineData[columnCounter] === '') {
-                    // fill empty cells with &nbsp;
-                    if (oFillEmptyCells) {
-                        tableLineData[columnCounter] = '&nbsp;';
-                    }
-
-                    emptyCellsCount++;
-                }
-            }
-
-            if (columnCounter > maxColumnsCount) {
-                maxColumnsCount = columnCounter;
-            }
-
-            const rowHtml = tableLineData.join(`</${tableCellTag}><${tableCellTag}>`);
-
-            tableData[rowCounter] = `${indent}<tr><${tableCellTag}>${rowHtml}</${tableCellTag}></tr>`;
-        }
-
-        const rowsHtml = tableData.join('\n');
-        const tableHtml = '<table>\n' + rowsHtml + '\n</table>';
-        const noResultText = 'No data available!\n\nPlease input some data into the textarea on the left.';
-
-        $('#code-output').textContent = tableData.length > 0 ? (oIncludeTableElement ? tableHtml : rowsHtml) : noResultText;
-        $('#code-preview').innerHTML = tableHtml;
-
-        // update information section
-        $('#aside-info-lines').innerText = rowCounter;
-        $('#aside-info-columns').innerText = maxColumnsCount;
-        $('#aside-info-empty-cells').innerText = emptyCellsCount;
-    }
-
-    static supportTab(event) {
-        if (event.keyCode === 9) {
-            event.preventDefault();
-
-            const start = event.currentTarget.selectionStart;
-            const end = event.currentTarget.selectionEnd;
-
-            event.currentTarget.value = event.currentTarget.value.substring(0, start) + '\t' + event.currentTarget.value.substring(end);
-            event.currentTarget.selectionStart = event.currentTarget.selectionEnd = start + 1;
-        }
-    }
-
-    forwardEvents() {
-        const evt = document.createEvent('HTMLEvents');
-
-        evt.initEvent('input', false, true);
-        this.inputArea.dispatchEvent(evt);
-    }
-
-    static changeTab(event) {
-        const clickedTab = event.currentTarget;
-        const tabElement = clickedTab.parentElement.parentElement;
-        const allTabs = tabElement.querySelectorAll('.tabs .tab');
-        const clickedTabIndex = nodeListIndexOf(allTabs, clickedTab);
-        const allTabBodyElements = tabElement.querySelectorAll('.tab-contents .tab-body');
-
-        for (let i = 0; i < allTabs.length; i++) {
-            allTabs[i].classList.remove('current');
-            allTabBodyElements[i].classList.remove('current');
-        }
-
-        allTabs[clickedTabIndex].classList.add('current');
-        allTabBodyElements[clickedTabIndex].classList.add('current');
-    }
+    allTabs[clickedTabIndex].classList.add('current')
+    allTabBodyElements[clickedTabIndex].classList.add('current')
+  }
 }
 
-new Tblr();
+new Tblr()
