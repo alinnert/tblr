5790c6fb4d766142cad30243a174132f2c61f5c1
Andreas Linnert <andreas@linnertmedia.de>
2020-08-11 19:28:33 +0200

    Rename files and add webmanifest

--- src/tblr.js
+++ src/tblr.js
@@ -25,7 +25,7 @@ const tblr = {
     tblr.convertInput()
   },
 
-  get version() { return '1.3.0' },
+  get version() { return '1.3.1' },
 
   prepareEvents () {
     tblr.inputArea.addEventListener(
